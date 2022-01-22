f617a0eb482b727b63640d57a51d0f9049d5f02f
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-01-22 18:25:02 +0900

    サンプルを追加

---
+++ C#/STPWithoutClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            Console.Write("STP>");
+            string cmd = Console.ReadLine();
+            string[] cmds = cmd.Split(' ');
+            switch(cmd)
+            {
+                case "exit":
+                    return;
+            }
+        }
+    }
+}
