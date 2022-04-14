cbbd98cb7ce1b62d73f6e990eb1f65fb007ca694
chroma7p <nahco3.hcl@gmail.com>
2022-04-15 08:45:31 +0900

    文字列への変数の挿入を変更

--- Python/STP_without_class/main.py
+++ Python/STP_without_class/main.py
@@ -13,18 +13,18 @@ while flag:
         item_names.append(name)
         item_values.append(value)
         item_counts.append(0)
-        print('{}円の{}を登録しました'.format(value, name))
+        print(f'{value}円の{name}を登録しました')
     if cmds[0] == 'show':
         for i in range(len(item_names)):
-            print('{}:{}:{}円'.format(i, item_names[i], item_values[i]))
+            print(f'{i}:{item_names[i]}:{item_values[i]}円')
     if cmds[0] == 'buy':
         target_index = int(cmds[1])
         item_counts[target_index] += 1
-        print('{}を購入しました'.format(item_names[target_index]))
+        print(f'{item_names[target_index]}を購入しました')
     if cmds[0] == 'checkout':
         sum = 0
         for i in range(len(item_names)):
             sum += item_values[i] * item_counts[i]
-            print('{} {}円×{}'.format(item_names[i], item_values[i], item_counts[i]))
+            print(f'{item_names[i]} {item_values[i]}円×{item_counts[i]}')
             item_counts[i] = 0
-        print('合計金額:{}円'.format(sum))
+        print(f'合計金額:{sum}円')
